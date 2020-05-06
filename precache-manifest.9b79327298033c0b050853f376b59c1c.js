012e36ede5ebffdb07081bbde8f841af22ff7f11
Work <work@SRs-iMac.local>
2020-05-06 11:26:00 +0200

    Updates

---
+++ precache-manifest.9b79327298033c0b050853f376b59c1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363110f5918b03c0c42cb4f48193cf13",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "38de1b80c741929b2403",
+    "url": "/memorygame/static/css/main.6bf07909.chunk.css"
+  },
+  {
+    "revision": "0612a3cda96b96f15b4e",
+    "url": "/memorygame/static/js/2.5d6152ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memorygame/static/js/2.5d6152ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38de1b80c741929b2403",
+    "url": "/memorygame/static/js/main.c64cf1bc.chunk.js"
+  },
+  {
+    "revision": "7668fafaa17705c6e2f4",
+    "url": "/memorygame/static/js/runtime-main.f879e122.js"
+  }
+]);
